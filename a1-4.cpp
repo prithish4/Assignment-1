11793a9694afd224cfeb30705b910e6d29623e8b
prithish4 <143925140+prithish4@users.noreply.github.com>
2024-09-25 22:50:41 +0530

    Update a1-4.cpp

--- a1-4.cpp
+++ a1-4.cpp
@@ -7,7 +7,35 @@ using namespace std;
 // Function to find the median
 double findMedian(int arr1[], int size1, int arr2[], int size2) {
     
-// Your code here
+int totalSize = size1 + size2;
+    int mergedIndex = 0;
+    int median1 = 0, median2 = 0;
+    int i = 0, j = 0;
+
+    // Merge elements from two sorted arrays until we reach the middle of the combined array
+for (mergedIndex = 0; mergedIndex <= totalSize / 2; mergedIndex++) {
+    // Store the previous median value before updating it
+    median1 = median2;
+
+    // Check if we have more elements in arr1 and either arr2 is empty 
+    // or the current element in arr1 is less than or equal to the current element in arr2
+    if (i < size1 && (j >= size2 || arr1[i] <= arr2[j])) {
+        // Take the next element from arr1 and move to the next index
+        median2 = arr1[i++];
+    } else {
+        // Otherwise, take the next element from arr2 and move to the next index
+        median2 = arr2[j++];
+    }
+}
+
+    // If the total number of elements is odd, return the single middle element
+    if (totalSize % 2 == 1) {
+        return median2;
+    } 
+    // If the total number of elements is even, return the average of the two middle elements
+    else {
+        return (median1 + median2) / 2.0;
+    }
 
 }
 
