2307a0bb855943d06574f02db28a3c13ea636ba8
prithish4 <143925140+prithish4@users.noreply.github.com>
2024-09-25 23:24:57 +0530

    Update a1-4.cpp

--- a1-4.cpp
+++ a1-4.cpp
@@ -6,49 +6,87 @@ using namespace std;
 
 // Function to find the median
 double findMedian(int arr1[], int size1, int arr2[], int size2) {
+int totalSize = size1 + size2;
+    int medianPos = (totalSize - 1) / 2;
     
-// Your code here
+    int i = 0, j = 0, count = 0;
+    int prev = -1, current = -1;
 
+    // Merge both arrays until we find the median position
+    while (i < size1 && j < size2) {
+        if (arr1[i] <= arr2[j]) {
+            prev = current;
+            current = arr1[i];
+            i++;
+        } else {
+            prev = current;
+            current = arr2[j];
+            j++;
+        }
+        
+        if (count == medianPos) {
+            if (totalSize % 2 == 0) {
+                return (current + prev) / 2.0;
+            } else {
+                return current;
+            }
+        }
+        count++;
+    }
+
+    // If elements remain in arr1
+    while (i < size1) {
+        prev = current;
+        current = arr1[i];
+        i++;
+        
+        if (count == medianPos) {
+            if (totalSize % 2 == 0) {
+                return (current + prev) / 2.0;
+            } else {
+                return current;
+            }
+        }
+        count++;
+    }
+
+    // If elements remain in arr2
+    while (j < size2) {
+        prev = current;
+        current = arr2[j];
+        j++;
+        
+        if (count == medianPos) {
+            if (totalSize % 2 == 0) {
+                return (current + prev) / 2.0;
+            } else {
+                return current;
+            }
+        }
+        count++;
+    }
+
+    return -1; // This line should never be reached
 }
 
-
 int main() {
-    // You can use cout statements, but ensure that you submit after commenting them out, as the autograder will otherwise mistake it for your actual output
-    // Everything else in the main function remains unchanged
-    
     int size1;
-    // cout << "Enter the size of the array1: ";
     cin >> size1;
 
     int arr[size1];
-    // cout << "Enter " << size1 << " elements:" << endl;
     for (int i = 0; i < size1; ++i) {
         cin >> arr[i];
     }
 
-    // cout << "The array elements are: ";
-    // for (int i = 0; i < size1; ++i) {
-    //     cout << arr[i] << " ";
-    // }
-    // cout << endl;
-
     int size2;
-    // cout << "Enter the size of the array2: ";
     cin >> size2;
 
     int arr1[size2];
-    // cout << "Enter " << size2 << " elements:" << endl;
     for (int i = 0; i < size2; ++i) {
         cin >> arr1[i];
     }
+    
+    cout << "Median = " << findMedian(arr, size1, arr1, size2) << endl;
 
-    // cout << "The array elements are: ";
-    // for (int i = 0; i < size2; ++i) {
-    //     cout << arr1[i] << " ";
-    // }
-    // cout << endl;
-    
-    cout << "Median = " << findMedian(arr, size1, arr1, size2) << endl; // do not comment this out
-    
     return 0;
 }
